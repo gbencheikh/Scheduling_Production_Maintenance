743ee16dfaf9c1bc2777deab3c5bed9f6120d786
BelgacemBettayeb <bbettayeb@cesi.fr>
2025-01-20 12:21:44 +0100

    20janvier-Experimentations

--- ComplexSystems/main.py
+++ ComplexSystems/main.py
@@ -16,9 +16,10 @@ import pandas as pd
 lambdaPM = [0.8]
 mu = [0]
 PM_time = [0]
-
-nbJobs, nbMachines, nbOperationsParJob, dureeOperations, processingTimes = parse_operations_file(f"TESTS/k2/k2.txt")
-_, _, nbComposants, seuils_degradation, dureeMaintenances, degradations, degradations2 = parse_degradations_file(f"TESTS/k2/instance02/instance.txt")
+n1=1 #numero d'instance Kacem
+n2=1 #numero d'instance machine
+nbJobs, nbMachines, nbOperationsParJob, dureeOperations, processingTimes = parse_operations_file(f"TESTS/k{n1}/k{n1}.txt")
+_, _, nbComposants, seuils_degradation, dureeMaintenances, degradations, degradations2 = parse_degradations_file(f"TESTS/k{n1}/instance{n2}/instance.txt")
 
 
 data = Data(nbJobs, nbMachines, nbComposants, seuils_degradation, dureeMaintenances, degradations, degradations2, nbOperationsParJob, dureeOperations, processingTimes)
@@ -32,17 +33,29 @@ iters=30000
 ResTest=[]
 weights=[0.7,0.2,0.1]
 ResTest=[]
-for alphakl in [0.00]:#,0.005,0.01]:
-    for qjmin in [0.85]:#,0.9,0.95]:
-        for lambdakl in [0.0]:
-            data.alpha_kl = [[alphakl for l in range(data.nbComposants[k])] for k in range(data.nbMachines)]
-            data.degradations=[[[[lambdakl  for ido in range(data.nbOperationsParJob[j])]  for j in range(data.nbJobs) ] for l in range(data.nbComposants[k])] for k in range(data.nbMachines)]
-            data.Qjmin = [qjmin for j in range(data.nbJobs)]        
-            optsolution, optCmax, cputime,nbrmaint,avgoq,qualpenal=rscs.Run_RSCS(tempInit,tempFin,coolRate,iters,weights,False)
-            save_JSON(data,optsolution,"testk2inst2.json",weights)
-            result = lire_fichier_json(f"testk2inst2.json")
-            plotGantt(result, "testk2inst2_figure",f"k2inst2-alpha{alphakl}-AQL{qjmin}", showgantt=False)
-            print([weights[0],weights[1],weights[2],optCmax, nbrmaint, avgoq, qualpenal,cputime])
-            ResTest.append([alphakl,qjmin,weights[0],weights[1],weights[2],optCmax, nbrmaint, avgoq,qualpenal,cputime])
+
+for qjmin in [0.85,0.9,0.95]:
+    optsolution, optCmax, cputime,nbrmaint,avgoq,qualpenal=rscs.Run_RSCS(tempInit,tempFin,coolRate,iters,weights,False)
+    save_JSON(data,optsolution,f"Results/testk{n1}inst{n2}.json",weights)
+    result = lire_fichier_json(f"Results/testk{n1}inst{n2}.json")
+    plotGantt(result, f"Results/testk{n1}inst{n2}_figure",f"k{n1}inst{n2}-alpha0-AQL{qjmin}", showgantt=False)
+    print([weights[0],weights[1],weights[2],optCmax, nbrmaint, avgoq, qualpenal,cputime])
+    ResTest.append(["Nan",qjmin,"Nan","Nan",weights[0],weights[1],weights[2],optCmax, nbrmaint, avgoq,qualpenal,cputime])
+
+for alphakl in [0.00,0.005,0.01]:
+    for qjmin in [0.85,0.9,0.95]:
+        for betakl in [0.0, 0.005, 0.01]:
+            for lambdakl in [0.85,0.9,0.95,1]:
+                print(f"alphak={alphakl} qjmin={qjmin} lambdakl={lambdakl}")
+                data.alpha_kl = [[alphakl for l in range(data.nbComposants[k])] for k in range(data.nbMachines)]
+                data.degradations=[[[[betakl  for ido in range(data.nbOperationsParJob[j])]  for j in range(data.nbJobs) ] for l in range(data.nbComposants[k])] for k in range(data.nbMachines)]
+                data.Qjmin = [qjmin for j in range(data.nbJobs)]    
+                data.seuils_degradation = [[lambdakl for l in range(data.nbComposants[k])] for k in range(data.nbMachines)]    
+                optsolution, optCmax, cputime,nbrmaint,avgoq,qualpenal=rscs.Run_RSCS(tempInit,tempFin,coolRate,iters,weights,False)
+                save_JSON(data,optsolution,f"Results/testk{n1}inst{n2}.json",weights)
+                result = lire_fichier_json(f"Results/testk{n1}inst{n2}.json")
+                plotGantt(result, f"Results/testk{n1}inst{n2}_figure",f"k{n1}inst{n2}-alpha{alphakl}-AQL{qjmin}", showgantt=False)
+                print([weights[0],weights[1],weights[2],optCmax, nbrmaint, avgoq, qualpenal,cputime])
+                ResTest.append([alphakl,qjmin,betakl,lambdakl,weights[0],weights[1],weights[2],optCmax, nbrmaint, avgoq,qualpenal,cputime])
 df=pd.DataFrame(ResTest)
-df.to_excel('ResOptWeightsk2inst2.xlsx', index=False, header=False)
+df.to_excel(f"Results/ResOptWeightsk{n1}inst{n2}.xlsx", index=False, header=False)
